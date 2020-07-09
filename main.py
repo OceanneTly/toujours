7ca8ec70a68272b8203e63f03b61cf7974299954
OceanneTly <61261637+OceanneTly@users.noreply.github.com>
2020-07-09 14:11:13 +1100

    Mise à jour main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-basic.show_string("Hello!")
+for index in range(4):
+    basic.show_string("Hello!")
+    basic.pause(200)
+    basic.show_number(10853)
+    basic.pause(200)
+    basic.show_arrow(ArrowNames.SOUTH)
+    basic.pause(200)
+    basic.show_icon(IconNames.HAPPY)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
-basic.showString("Hello!")
-basic.showNumber(10853)
-basic.showArrow(ArrowNames.South)
-basic.showIcon(IconNames.Happy)
+for (let index = 0; index < 4; index++) {
+    basic.showString("Hello!")
+    basic.pause(500)
+    basic.showNumber(10853)
+    basic.pause(500)
+    basic.showArrow(ArrowNames.South)
+    basic.pause(500)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(500)
+}
 basic.forever(function () {
 	
 })
